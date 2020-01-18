9ed8c0be64a1d2b4ee2b28d73e4a8cef4b7303f3
janithalgewatta <janith2011@gmail.com>
2020-01-18 09:53:51 +0530

    add user management end points

---
+++ app/user/views.py
@@ -0,0 +1,26 @@
+from rest_framework import generics, authentication, permissions
+from user.serializers import UserSerializer, AuthTokenSerializer
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+
+
+class CreateUserView(generics.CreateAPIView):
+    """create new user in a system"""
+    serializer_class = UserSerializer
+
+
+class CreateTokenView(ObtainAuthToken):
+    """Create a new auth token for user"""
+    serializer_class = AuthTokenSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """Manage Authenticate User"""
+    serializer_class = UserSerializer
+    authentication_classes = (authentication.TokenAuthentication,)
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_object(self):
+        """Retrive and Return authenticate user"""
+        return self.request.user
