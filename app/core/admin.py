ce2b5675c97d76c68c3c229aa4e385c066f8c78b
janithalgewatta <janith2011@gmail.com>
2020-01-19 21:03:51 +0530

    ingredience api

--- app/core/admin.py
+++ app/core/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext as _
 from core import models
+
+
 # models import
 
 
@@ -10,12 +12,12 @@ class UserAdmin(BaseUserAdmin):
     list_display = ['email', 'name']
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
-        (_('Personal Info'), {'fields': ('name', )}),
+        (_('Personal Info'), {'fields': ('name',)}),
         (
             _('Permissions'),
             {'fields': ('is_active', 'is_staff', 'is_superuser')}
         ),
-        (_('Important Dates'), {'fields': ('last_login', )})
+        (_('Important Dates'), {'fields': ('last_login',)})
     )
     add_fieldsets = (
         (None, {
@@ -26,3 +28,5 @@ class UserAdmin(BaseUserAdmin):
 
 
 admin.site.register(models.User, UserAdmin)
+admin.site.register(models.Tag)
+admin.site.register(models.Ingredient)
